f4675ff21e348c58c86392e4449c83e9e7847ce3
Protik Banerji <protik09@gmail.com>
2019-06-26 23:53:41 +0530

    Work out a bit of the project structure and include header and test folders

---
+++ include/proc_manager_main.hpp
@@ -0,0 +1,29 @@
+/**
+ * \file proc_manager_main.hpp
+ *
+ * \brief The main program for the proc_manager
+ *
+ * \details A better task manager for linux
+ *
+ * \author Protik Banerji <protik09@gmail.com>
+ */
+
+#ifndef PROC_MANAGER_MAIN_H
+#define PROC_MANAGER_MAIN_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/proc_manager_typedefs.hpp"
+
+// #ifdef __cplusplus
+// extern "C"
+// {
+// #endif
+
+
+// #ifdef __cplusplus
+// }
+// #endif
+
+#endif /* PROC_MANAGER_MAIN_H */
